52a94442edcba83f9fa46c87662df64b0112992c
evan kruger <ekruger713@gmail.com>
2020-03-06 05:35:03 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,9 @@
-def square_array(array)
-  # your code here
+def square_array(num)
+  num = [1,2,3]
+    counter = 0
+ 
+  while num[counter] do
+    sqrt(num)[counter]
+    counter += 1
+  end
 end

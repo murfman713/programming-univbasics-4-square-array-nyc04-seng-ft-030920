db04b26c123db97d97b2a3089a660dbcf11f4cd9
evan kruger <ekruger713@gmail.com>
2020-03-06 05:56:55 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -5,12 +5,3 @@ while numbers[counter] do
   puts numbers[counter] ** 2
   counter += 1
 end
-
- 
-new_numbers = [9,10,16,25]
-counter = 0
-
-while new_numbersnumbers[counter] do
-  puts new_numbersnumbers[counter] ** 2
-  counter += 1
-end
